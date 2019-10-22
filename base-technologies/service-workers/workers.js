9a0b4609e71fca2ceb9a56fa7bd5598445bb65ab
yututi <y.tsuchiya0207@gmail.com>
2019-10-22 20:00:44 +0900

    service worker

--- base-technologies/service-workers/workers.js
+++ base-technologies/service-workers/workers.js
@@ -1,13 +1,19 @@
 const cacheName = "super-duper-cache"
-const urlsToCache = []
+const urlsToCache = [
+    "/base-technologies/service-workers/index.html",
+    "/base-technologies/service-workers/index.js"
+]
 
-self.addEventListener("install", () => {
-
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(cacheName).then((cache) => cache.addAll(urlsToCache))
+    )
 })
 
 self.addEventListener("fetch", (event) => {
     event.respondWith(caches.match(event.request).then((response) => {
         if (response) {
+            console.log("fetched from caches.")
             return response
         }
         return fetch(event.request)
